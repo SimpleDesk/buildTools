0c9ba8e09d95efa50f07cdd289251f9414c5e749
jdarwood007 <unmonitored+github@sleepycode.com>
2017-09-03 08:13:44 -0700

    Initial commit of tools
    Signed-off-by: jdarwood007 <unmonitored+github@sleepycode.com>

---
+++ check-eof.php
@@ -0,0 +1,59 @@
+<?php
+###############################################################
+#          Simple Desk Project - www.simpledesk.net           #
+###############################################################
+#       An advanced help desk modification built on SMF       #
+###############################################################
+#                                                             #
+#         * Copyright 2017 - SimpleDesk.net                   #
+#                                                             #
+#   This file and its contents are subject to the license     #
+#   included with this distribution, license.txt, which       #
+#   states that this software is New BSD Licensed.            #
+#   Any questions, please contact SimpleDesk.net              #
+#                                                             #
+###############################################################
+# SimpleDesk Version: 2.1 Beta 1                              #
+# File Info: check-eof.php                                    #
+###############################################################
+
+// Stuff we will ignore.
+$ignoreFiles = array(
+	// Build tools
+	'./buildtools/[A-Za-z0-9-_]+.php',
+);
+
+// No file? Thats bad.
+if (!isset($_SERVER['argv'], $_SERVER['argv'][1]))
+	die('Error: No File specified' . "\n");
+
+// The file has to exist.
+$currentFile = $_SERVER['argv'][1];
+if (!file_exists($currentFile))
+	die('Error: File does not exist' . "\n");
+
+// Is this ignored?
+foreach ($ignoreFiles as $if)
+	if (preg_match('~' . $if . '~i', $currentFile))
+		die;
+
+// Less efficent than opening a file with fopen, but we want to be sure to get the right end of the file. file_get_contents
+$file = fopen($currentFile, 'r');
+
+// Seek the end minus some bytes.
+fseek($file, -100, SEEK_END);
+$contents = fread($file, 100);
+
+// There is some white space here.
+if (preg_match('~}\s+$~', $contents, $matches))
+	die('Error: End of File contains extra spaces in ' . $currentFile . "\n");
+// It exists! Leave.
+elseif (preg_match('~}$~', $contents, $matches))
+	die();
+
+// There is some white space here.
+if (preg_match('~\';\s+$~', $contents, $matches))
+	die('Error: End of File Strings contains extra spaces in ' . $currentFile . "\n");
+// It exists! Leave.
+elseif (preg_match('~\';$~', $contents, $matches))
+	die();
---
+++ runTools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git submodule foreach git pull origin master
+
+if [ $# -eq 0 ]
+then
+	echo "Running Local Version"
+	find . -name "*.php" ! -path "./vendor/*" -exec php -l {} 2>&1 \; | grep "syntax error, unexpected";
+	php other/buildTools/check-signed-off.php | grep "Error:";
+	find . -name "*.php" -exec php buildTools/check-license.php {} 2>&1 \; | grep "Error:";
+	find . -name "*.php" -exec php buildTools/check-eof.php {} 2>&1 \; | grep "Error:";
+else
+	echo "Running Travis Version"
+	if find . -name "*.php" ! -path "./vendor/*" -exec php -l {} 2>&1 \; | grep "syntax error, unexpected"; then exit 1; fi
+	if php other/buildTools/check-signed-off.php travis | grep "Error:"; then php buildTools/check-signed-off.php travis; exit 1; fi
+	if find . -name "*.php" -exec php buildTools/check-license.php {} 2>&1 \; | grep "Error:"; then exit 1; fi
+	if find . -name "*.php" -exec php buildTools/check-eof.php {} 2>&1 \; | grep "Error:"; then exit 1; fi
+fi
