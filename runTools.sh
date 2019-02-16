ddfd35fba4bacf0a64d80774e06c5a054d1a252c
jdarwood007 <unmonitored+github@sleepycode.com>
2019-02-16 11:46:50 -0800

    Ensure that build tools fail at the right step locally
    Signed-off-by: jdarwood007 <unmonitored+github@sleepycode.com>

--- runTools.sh
+++ runTools.sh
@@ -6,9 +6,13 @@ if [ $# -eq 0 ]
 then
 	echo "Running Local Version"
 	find . -name "*.php" ! -path "./vendor/*" -exec php -l {} 2>&1 \; | grep "syntax error, unexpected";
+        if find . -name "*.php" ! -path "./vendor/*" -exec php -l {} 2>&1 \; | grep "syntax error, unexpected"; then exit 1; fi
 	php other/buildTools/check-signed-off.php | grep "Error:";
+        if php other/buildTools/check-signed-off.php travis | grep "Error:"; then php buildTools/check-signed-off.php travis; exit 1; fi
 	find . -name "*.php" -exec php buildTools/check-license.php {} 2>&1 \; | grep "Error:";
+        if find . -name "*.php" -exec php buildTools/check-license.php {} 2>&1 \; | grep "Error:"; then exit 1; fi
 	find . -name "*.php" -exec php buildTools/check-eof.php {} 2>&1 \; | grep "Error:";
+        if find . -name "*.php" -exec php buildTools/check-eof.php {} 2>&1 \; | grep "Error:"; then exit 1; fi
 else
 	echo "Running Travis Version"
 	if find . -name "*.php" ! -path "./vendor/*" -exec php -l {} 2>&1 \; | grep "syntax error, unexpected"; then exit 1; fi
